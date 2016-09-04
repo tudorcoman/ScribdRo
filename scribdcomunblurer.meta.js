817a44f74a768529ad46f22c229ba1a07233e250
Tudor Coman <tudor.a.coman@gmail.com>
2016-09-04 21:47:51 +0300

    Mistake in metadata

--- scribdcomunblurer.meta.js
+++ scribdcomunblurer.meta.js
@@ -8,7 +8,7 @@
 // @include        https://ro.scribd.com/document/*
 // @run-at         document-end
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @homepageURL    https://github.com/tudorcoman/Hack-Scribd-Unblurer
+// @homepageURL    https://github.com/tudorcoman/ScribdRo
 // @iconURL        http://www.scribd.com/favicon.ico
 // @supportURL     https://github.com/tudorcoman/ScribdRo
 // @updateURL      https://github.com/tudorcoman/ScribdRo/blob/master/scribdcomunblurer.meta.js
